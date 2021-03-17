d9196cbf94962f0db99c96a6c395767435d361bc
trojano <marcos.trajano@acad.pucrs.br>
2021-03-17 12:55:25 -0300

    mudança da posiçao do email

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "44ba9e4436f6e64a31bb";
+/******/ 	var hotCurrentHash = "5574d800ed3eda87a34c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
